c633c7e0bb7271d9ecbb08d5f1036807606bc9b0
serdimoa <serdimoa@gmail.com>
2015-10-03 19:51:24 +0500

    many page

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -12,48 +12,18 @@
                 $(tabs).children("ul").children("li").eq(i).addClass("active");
             };
 
-            hidePage = function (i) {
-
-                $(tabs).children("div").children("div").hide(0);
-                $(tabs).children("div").removeClass("borders");
-
-            };
-
-            delActive = function (i) {
-
-                $(tabs).children("ul").children("li").removeClass("active");
-
-            };
-
-            selectPage = function (i) {
-
-                $(tabs).children("div").addClass("borders");
-                $(tabs).children("div").children("div").eq(i).show(1000);
-
-            };
-
-            showPage(0);
-            delActive(0);
-            hidePage(0);
-
-
             $(tabs).children("ul").children("li").each(function (index, element) {
                 $(element).attr("data-page", i);
                 i++;
             });
 
-            $(tabs).children("ul").children("li").hover(function () {
-                if (parseInt($(this).attr("data-page")) != 4)
-                    selectPage(parseInt($(this).attr("data-page")));
-            });
 
-            $(tabs).children("ul").children("li").mouseout(function () {
-                hidePage(parseInt($(this).attr("data-page")));
-            });
+
+
 
             $(tabs).children("ul").children("li").click(function () {
                 $('#content').show(1000);
-                $('.price').css( "opacity", 1 );
+                $('.price').css("opacity", 1);
                 showPage(parseInt($(this).attr("data-page")));
                 var calcs = new Calcinit();
                 calcs.init(parseInt($(this).attr("data-id")));
@@ -62,27 +32,47 @@
         return this.each(createTabs);
     };
 })(jQuery);
-var calculate = function(){
+var calculate = function () {
     var initcalc = new Calcinit();
     var price = $('.price');
     var tabs = $('.tabs');
     tabs.lightTabs();
-    $(".button-collapse").sideNav();
-    price.pushpin({top: price.offset().top, offset: 54});
-    tabs.pushpin({top: tabs.offset().top});
+    price.pushpin({top: price.offset().top, offset: 154});
+    tabs.pushpin({top: tabs.offset().top,offset:100});
 };
 $(document).ready(function () {
+    $(".button-collapse").sideNav( {menuWidth: 300, // Default is 240
+        edge: 'right', // Choose the horizontal origin
+        closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+});
 
-    if($("#home")){
-        var menu = $(".menu");
-        $(".parallax").parallax();
-        menu.pushpin({top: menu.offset().top});
+    var n = new Date().getFullYear();
+    $("#year").text(n);
+    var element = document.getElementById('index-banner');
+    var element2 = document.getElementById('conts');
+    var menu = $(".menu");
+    menu.pushpin({top: menu.offset().top});
+    if (!element&&!element2) {
+        var c = document.getElementById("menu_calc");
+        c.className = c.className + " activemenu";
+        calculate();
+        console.log("other");
+
+    } else if(element&&!element2)
+    {
+        var h = document.getElementById("menu_home");
+        h.className = h.className + " activemenu";
         console.log("home");
     }
-    else{
-        console.log("other");
-        calculate();
+    else if(element2&&!element){
+        var d = document.getElementById("menu_contact");
+        d.className = d.className + " activemenu";
+        $('#textarea').trigger('autoresize');
+        console.log("hellppp")
     }
+
+
+
 
 });
 
