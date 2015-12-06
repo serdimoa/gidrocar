55f4e45c75bc3505283c200ffbcd34421fdff684
serdimoa <serdimoa@gmail.com>
2015-12-06 22:33:58 +0500

    rr

--- src/js/partials/calc.js
+++ src/js/partials/calc.js
@@ -6,8 +6,8 @@ var calc_price = 0;
 var calc_priceBefore = $(".price-before span ");
 var calc_priceAfter = $(".price-after span ");
 /**
- * Оюъект цен
-  */
+ * пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ
+ */
 
 var Category_value = function (base, salon, him, engine, coleso, discount) {
 
@@ -22,40 +22,40 @@ var Category_value = function (base, salon, him, engine, coleso, discount) {
 };
 
 /**
- * Функция добавления value
+ * пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ value
  */
 
 Category_value.prototype.init = function () {
     this.base.forEach(function (value, i) {
         i += 1;
-        $("#base-" + i+" input").val(value);
-        $("#base-" + i+" span").html(value + '<i class="fa fa-rub"></i>');
+        $("#base-" + i + " input").val(value);
+        $("#base-" + i + " span").html(value + '<i class="fa fa-rub"></i>');
     });
 
     this.salon.forEach(function (value, i) {
         i += 1;
-        $("#salon-" + i+" input").val(value);
-        $("#salon-" + i+" span").html(value + '<i class="fa fa-rub"></i>');
+        $("#salon-" + i + " input").val(value);
+        $("#salon-" + i + " span").html(value + '<i class="fa fa-rub"></i>');
     });
 
     this.him.forEach(function (value, i) {
         i += 1;
-        $("#chim-salon-" + i+" input").val(value);
-        $("#chim-salon-" + i+" span").html(value + '<i class="fa fa-rub"></i>');
+        $("#chim-salon-" + i + " input").val(value);
+        $("#chim-salon-" + i + " span").html(value + '<i class="fa fa-rub"></i>');
 
     });
 
     this.engine.forEach(function (value, i) {
         i += 1;
-        $("#engine-" + i+" input").val(value);
-        $("#engine-" + i+" span").html(value + '<i class="fa fa-rub"></i>');
+        $("#engine-" + i + " input").val(value);
+        $("#engine-" + i + " span").html(value + '<i class="fa fa-rub"></i>');
 
     });
 
     this.coleso.forEach(function (value, i) {
         i += 1;
-        $("#coleso-" + i+" input").val(value);
-        $("#coleso-" + i+" span").html(value + '<i class="fa fa-rub"></i>');
+        $("#coleso-" + i + " input").val(value);
+        $("#coleso-" + i + " span").html(value + '<i class="fa fa-rub"></i>');
 
     });
 
@@ -74,7 +74,7 @@ Category_value.prototype.init = function () {
 };
 
 /**
- *  Класс калькулятора
+ *  пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
  * @param param
  * @constructor
  */
@@ -82,12 +82,12 @@ Category_value.prototype.init = function () {
 var Calcinit = function () {
 
 
-    //Обработка чекбоксов
-    //Жмем
+    //пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
+    //пїЅпїЅпїЅпїЅ
     $(":checkbox").on('change', function () {
         if ($(this).prop("checked")) {
             var selected = [];
-            //если не нажат то добавляем стоимость
+            //пїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
             if ($(":radio:checked")) {
                 price_change($(":radio:checked").val());
 
@@ -104,7 +104,7 @@ var Calcinit = function () {
 
         }
         else {
-            //если нажат уменьшаем
+            //пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
             Radio_check();
             $(this).prop("checked", false);
             if ($(":radio:checked")) {
@@ -118,7 +118,7 @@ var Calcinit = function () {
         }
     });
     /**
-     * Обработка радиобатонов
+     * пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
      */
     $(":radio").on('change', function () {
         switch ($(this).attr("id")) {
@@ -145,47 +145,43 @@ var Calcinit = function () {
         }
     });
     /**
-     * Обработка при нажатии комлекса
+     * пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
      * @param e
      */
     function complex(e) {
         $("#base-3 input,#salon-4 input,#salon-7 input,#salon-1 input,#salon-3 input,#salon-6 input,#coleso-1 input").prop("checked", false);
 
         e.base.forEach(function (val) {
-            $("#base-" + val+" input").prop("checked", true);
+            $("#base-" + val + " input").prop("checked", true);
         });
         e.salon.forEach(function (val) {
-            $("#salon-" + val +" input").prop("checked", true);
+            $("#salon-" + val + " input").prop("checked", true);
         });
         if (e.koleso) {
             e.koleso.forEach(function (val) {
-                $("#coleso-" + val+" input").prop("checked", true);
+                $("#coleso-" + val + " input").prop("checked", true);
             });
 
         }
 
 
-
     }
-
-
-
 
 
 };
 
 /**
- * Функция очистки
+ * пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ
  */
 Calcinit.prototype.clean = function () {
 
-        $(":checkbox").prop('checked', false);
-        $(":radio").prop('checked', false);
+    $(":checkbox").prop('checked', false);
+    $(":radio").prop('checked', false);
     price_change();
 
 };
 /**
- * Функция инициализации значений по категориям
+ * пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
  * @param param
  *
  */
@@ -194,80 +190,86 @@ Calcinit.prototype.init = function (param) {
     switch (this.param) {
         case 1:
             var category_1 = new Category_value(
-                [150, 200, 350, 100], [80, 50, 100, 100, 100, 100, 50, 50], [1200, 1100, 500, 500, 1000, 1000], [100, 350], [100, 30], [130,130, 130]);
+                [200, 250, 50, 50, 50], [60, 80, 100, 100, 100, 50, 50], [1100, 1000, 500, 500, 1000, 1000], [100, 250], [100, 50], [40, 40, 40]);
             category_1.init();
             break;
         case 2:
             var category_2 = new Category_value(
-                [170, 220, 400, 100], [100, 60, 100, 100, 110, 110, 50, 50], [1300, 1200, 600, 600, 1100, 1100], [100, 400], [100, 40], [160, 160, 160]);
+                [250, 300, 60, 50, 50], [90, 110, 100, 100, 50, 50], [1200, 1100, 500, 500, 1000, 1000], [100, 300], [100, 50], [80, 80, 80]);
             category_2.init();
             break;
         case 3:
             var category_3 = new Category_value(
-                [190, 240, 450, 110], [110, 70, 110, 100, 120, 120, 50, 50], [1500, 1500, 700, 700, 1500, 1400], [100, 450], [120, 40], [190, 190, 190]);
+                [300, 350, 70, 50, 50], [80, 100, 120, 100, 110, 50, 50], [1300, 1200, 600, 600, 1100, 1100], [100, 350], [100, 50], [100, 100, 100]);
             category_3.init();
             break;
         case 4:
             var category_4 = new Category_value(
-                [210, 260, 500, 120], [180, 70, 140, 100, 140, 130, 50, 50], [1700, 1600, 800, 800, 1800, 1800], [100, 500], [130, 50], [140, 170, 200]);
+                [350, 400, 90, 60, 50], [100, 110, 130, 100, 120, 50, 50], [1400, 1300, 700, 700, 1200, 1200], [100, 400], [110, 60], [120, 120, 120]);
             category_4.init();
             break;
         case 5:
             var category_5 = new Category_value(
-                [230, 280, 550, 130], [200, 80, 150, 100, 140, 150, 50, 50], [1800, 1700, 900, 900, 1900, 1900], [100, 800], [140, 60], [180, 210, 220]);
+                [450, 500, 100, 60, 50], [100, 150, 160, 100, 140, 50, 50], [1700, 1600, 800, 800, 1800, 1800], [100, 500], [130, 70], [150, 150, 150]);
             category_5.init();
+            break;
+        case 6:
+            var category_6 = new Category_value(
+                [500, 550, 110, 60, 50], [110, 250, 200, 100, 150, 50, 50], [1800, 1700, 900, 900, 1900, 1900], [100, 550], [140, 70], [240, 240, 240]);
+            category_6.init();
             break;
     }
 };
 
 /**
- * 1 комплекс, указывает на id чекбокса который нужно нажать
+ * 1 пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ, пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ id пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ
  * @type {{base: number[], salon: number[]}}
  */
-var complex_1_value = {base: [3], salon: [7, 2,4, 1, 3]};
+var complex_1_value = {base: [2,3], salon: [6, 2, 3, 1]};
 
 /**
- * 2 комплекс, указывает на id чекбокса который нужно нажать
+ * 2 пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ, пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ id пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ
  * @type {{base: number[], salon: number[]}}
  */
-var complex_2_value = {base: [3], salon: [7, 2,4, 1, 3, 6]};
+var complex_2_value = {base: [2,3], salon: [6, 2, 3, 1, 5]};
 
 /**
- * 3 комплекс, указывает на id чекбокса который нужно нажать
+ * 3 пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ, пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ id пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ
  * @type {{base: number[], salon: number[], koleso: number[]}}
  */
-var complex_3_value = {base: [3], salon: [7, 2,4, 1, 3, 6], koleso: [1]};
+var complex_3_value = {base: [2,3], salon: [6, 2, 3, 1, 5], koleso: [1]};
 
 /**
- * Проверка чекбоксов для включения скидок
+ * пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ
  * @constructor
  */
 function Radio_check() {
     if ($("#base-3 input").prop("checked") &&
-        $("#salon-4 input").prop("checked") &&
-        $("#salon-7 input").prop("checked") &&
-        $("#salon-1 input").prop("checked") &&
-        $("#salon-3 input").prop("checked") && !$("#salon-6 input").prop("checked") && !$("#coleso-1 input").prop("checked")
+        $("#base-2 input").prop("checked") &&
+        $("#salon-6 input").prop("checked") &&
+        $("#salon-2 input").prop("checked") &&
+        $("#salon-3 input").prop("checked") &&
+        $("#salon-5 input").prop("checked") && !$("#salon-5 input").prop("checked") && !$("#coleso-1 input").prop("checked")
     ) {
         $("#complex-1 ").prop("checked", true);
         price_change($("#complex-1").val());
     }
     else if ($("#base-3 input").prop("checked") &&
-        $("#salon-4 input").prop("checked") &&
-        $("#salon-7 input").prop("checked") &&
-        $("#salon-1 input").prop("checked") &&
+        $("#base-2 input").prop("checked") &&
+        $("#salon-6 input").prop("checked") &&
+        $("#salon-2 input").prop("checked") &&
         $("#salon-3 input").prop("checked") &&
-        $("#salon-6 input").prop("checked") && !$("#coleso-1 input").prop("checked")
+        $("#salon-5 input").prop("checked") && !$("#coleso-1 input").prop("checked")
     ) {
         $("#complex-2 ").prop("checked", true);
         price_change($("#complex-2").val());
     }
     else if ($("#base-3 input").prop("checked") &&
-        $("#salon-4 input").prop("checked") &&
-        $("#salon-7 input").prop("checked") &&
-        $("#salon-1 input").prop("checked") &&
-        $("#salon-3 input").prop("checked") &&
+        $("#base-2 input").prop("checked") &&
         $("#salon-6 input").prop("checked") &&
+        $("#salon-2 input").prop("checked") &&
+        $("#salon-3 input").prop("checked") &&
+        $("#salon-5 input").prop("checked") &&
         $("#coleso-1 input").prop("checked")
     ) {
 
@@ -289,10 +291,10 @@ function Radio_check() {
 
 }
 /**
- * Проверкаизменения цен, и включения скидочного блока
+ * пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ, пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ
  * @param vals
  */
-function price_change (vals) {
+function price_change(vals) {
     calc_price = 0;
     var discount = vals;
     $(":checkbox").each(function (e) {
